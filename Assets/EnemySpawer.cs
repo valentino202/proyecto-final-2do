3cbcc88ea5dce9ea1f2255504cbfd465f196aacb
valentino202 <valentinolozanopalomino@gmail.com>
2025-10-16 13:34:06 -0500

    a
    a

---
+++ Assets/EnemySpawer.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class EnemySpawer : MonoBehaviour
+{
+   public GameObject EnemyPrefab;
+   [SerializeField]private float timeTospawn = 5;
+   private float currenntnay;
+  
+
+
+
+
+}
