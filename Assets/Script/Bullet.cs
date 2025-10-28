f63621523f2bf0d4cbfd9a0603083d8cc1817457
valentino202 <valentinolozanopalomino@gmail.com>
2025-10-28 15:21:23 -0500

    AVACEN DE REBOTE MAS FINO
    0W0

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -3,6 +3,46 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-  
+  Rigidbody2D rb;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bounce"))
+        {
+            Debug.Log("la bala colicciono con el " + collision.gameObject + " puede rebotar");
+
+            ContactPoint2D contact = collision.contacts[0];
+            print("Punto de contacto: " + contact.point);
+
+
+            Vector2 vl = rb.linearVelocity;
+            Vector2 normal = collision.contacts[0].normal;
+
+
+             rb.linearVelocity = Vector2.zero;
+            Debug.Log("la velocidad se mantine zero");
+
+            float magnitud = vl.magnitude;
+            Vector2 nuevaDireccion = Vector2.zero;
+
+            if (Mathf.Abs(normal.x) == Mathf.Abs(normal.y))
+            {
+
+            }
+
+
+        }
+        else if (!collision.gameObject.CompareTag("Bounce"))
+        {
+            Debug.Log("la bala no puede rebotar en "+ collision.gameObject);
+            Destroy(gameObject);
+        }
+
+    }
 }
